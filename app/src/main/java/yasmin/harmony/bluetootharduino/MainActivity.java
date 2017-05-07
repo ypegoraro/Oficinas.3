2ec319bcda8a738f1735d0d2b514df5778af7201
Yasmin Pegoraro <yasminpegoraro@alunos.utfpr.edu.br>
2017-05-07 15:01:40 -0300

    Initial commit

---
+++ app/src/main/java/yasmin/harmony/bluetootharduino/MainActivity.java
@@ -0,0 +1,208 @@
+package yasmin.harmony.bluetootharduino;
+
+import android.content.res.AssetFileDescriptor;
+import android.media.AudioManager;
+import android.media.MediaPlayer;
+import android.media.SoundPool;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.bluetooth.BluetoothAdapter;
+import android.os.Bundle;
+import android.os.Handler;
+import android.os.Message;
+import android.util.Log;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import java.io.File;
+import java.io.IOException;
+
+public class MainActivity extends AppCompatActivity{
+
+    /* Definição dos objetos que serão usados na Activity Principal
+        statusMessage mostrará mensagens de status sobre a conexão
+        counterMessage mostrará o valor do contador como recebido do Arduino
+        connect é a thread de gerenciamento da conexão Bluetooth
+     */
+    static TextView statusMessage;
+    static TextView counterMessage;
+    ConnectionThread connect;
+    Button btn;
+    static MediaPlayer mp;
+    static SoundPool sp;
+    static private int soundID;
+    static boolean plays = false, loaded = false;
+    static float actVolume, maxVolume, volume;
+    AudioManager audioManager;
+    static int counter;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        /* Link entre os elementos da interface gráfica e suas
+            representações em Java.
+         */
+        statusMessage = (TextView) findViewById(R.id.statusMessage);
+        counterMessage = (TextView) findViewById(R.id.counterMessage);
+        btn = (Button) findViewById(R.id.button);
+        mp = new MediaPlayer();
+        mp.setOnPreparedListener(new MediaPlayer.OnPreparedListener(){
+            @Override
+            public void onPrepared(MediaPlayer mp){
+                mp.start();
+            }
+        });
+
+       // btn.setOnClickListener(this);
+
+        audioManager = (AudioManager) getSystemService(AUDIO_SERVICE);
+        actVolume = (float) audioManager.getStreamVolume(AudioManager.STREAM_MUSIC);
+        maxVolume = (float) audioManager.getStreamMaxVolume(AudioManager.STREAM_MUSIC);
+        volume = actVolume / maxVolume;
+
+        counter = 0;
+
+        sp = new SoundPool(10, AudioManager.STREAM_MUSIC, 0);
+        sp.setOnLoadCompleteListener(new SoundPool.OnLoadCompleteListener() {
+            @Override
+            public void onLoadComplete(SoundPool sp, int sampleId, int status) {
+                loaded = true;
+            }
+        });
+        soundID = sp.load(this, R.raw.som, 1);
+
+
+
+        /* Teste rápido. O hardware Bluetooth do dispositivo Android
+            está funcionando ou está bugado de forma misteriosa?
+            Será que existe, pelo menos? Provavelmente existe.
+         */
+        BluetoothAdapter btAdapter = BluetoothAdapter.getDefaultAdapter();
+        if (btAdapter == null) {
+            statusMessage.setText("Que pena! Hardware Bluetooth não está funcionando :(");
+        } else {
+            statusMessage.setText("Ótimo! Hardware Bluetooth está funcionando :)");
+        }
+
+        /* A chamada do seguinte método liga o Bluetooth no dispositivo Android
+            sem pedido de autorização do usuário. É altamente não recomendado no
+            Android Developers, mas, para simplificar este app, que é um demo,
+            faremos isso. Na prática, em um app que vai ser usado por outras
+            pessoas, não faça isso.
+         */
+        btAdapter.enable();
+
+        /* Definição da thread de conexão como cliente.
+            Aqui, você deve incluir o endereço MAC do seu módulo Bluetooth.
+            O app iniciará e vai automaticamente buscar por esse endereço.
+            Caso não encontre, dirá que houve um erro de conexão.
+         */
+        connect = new ConnectionThread("98:D3:31:90:60:7A");
+        connect.start();
+
+        /* Um descanso rápido, para evitar bugs esquisitos.
+         */
+        try {
+            Thread.sleep(1000);
+        } catch (Exception E) {
+            E.printStackTrace();
+        }
+    }
+
+   /* @Override
+    public void onClick(View v) {
+        restartCounter(v);
+    }*/
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.menu_main_bluetooth, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+
+        //noinspection SimplifiableIfStatement
+        if (id == R.id.action_settings) {
+            return true;
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
+
+    public static Handler handler = new Handler() {
+        @Override
+        public void handleMessage(Message msg) {
+
+            /* Esse método é invocado na Activity principal
+                sempre que a thread de conexão Bluetooth recebe
+                uma mensagem.
+             */
+            Bundle bundle = msg.getData();
+            byte[] data = bundle.getByteArray("data");
+            String dataString = new String(data);
+
+            /* Aqui ocorre a decisão de ação, baseada na string
+                recebida. Caso a string corresponda à uma das
+                mensagens de status de conexão (iniciadas com --),
+                atualizamos o status da conexão conforme o código.
+             */
+            if (dataString.equals("---N"))
+                statusMessage.setText("Ocorreu um erro durante a conexão D:");
+            else if (dataString.equals("---S"))
+                statusMessage.setText("Conectado :D");
+            else if (dataString.equals("t1")) {
+
+                /*if (mp != null) {
+                    mp.reset();
+                    mp.release();
+                }
+                mp = MediaPlayer.create(MainActivity.this, R.raw.som);
+
+                mp.start();*/
+
+                if (loaded && !plays) {
+                    sp.play(soundID, volume, volume, 1, 0, 1f);
+                    counter = counter++;
+                    plays = true;
+                }
+            }
+        }
+    };
+
+                /* Se a mensagem não for um código de status,
+                    então ela deve ser tratada pelo aplicativo
+                    como uma mensagem vinda diretamente do outro
+                    lado da conexão. Nesse caso, simplesmente
+                    atualizamos o valor contido no TextView do
+                    contador.
+                 */
+              //  counterMessage.setText(dataString);
+
+
+
+    }
+
+    /* Esse método é invocado sempre que o usuário clicar na TextView
+        que contem o contador. O app Android transmite a string "restart",
+        seguido de uma quebra de linha, que é o indicador de fim de mensagem.
+     */
+    /*public void restartCounter(View view) {
+
+        connect.write("restart\n".getBytes());
+    }*/
+
+
+
